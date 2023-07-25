767d13d7b2bdef3bafc49e581f5deb3495da7b6d
ZeroClian <zeroycl@163.com>
2023-07-25 22:42:19 +0800

    feat: 集成mock、axios

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,21 @@
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import request from '@/utils/request'
+import { onMounted } from 'vue'
+onMounted(() => {
+  request({
+    url: '/user/login',
+    method: 'post',
+    data: {
+      username: 'admin',
+      password: '111111',
+    },
+  }).then((res) => {
+    console.log(res)
+  })
+})
+</script>
 
 <style scoped lang="scss"></style>
